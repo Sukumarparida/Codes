c9635704eea3330e73e4a16678a110639f9830d0
Sukumar Parida <sukumarparida77@gmail.com>
2026-04-01 11:22:17 +0530

    Time: 57 ms (60.08%), Space: 194.9 MB (49.1%) - LeetHub

---
+++ 2751-robot-collisions/2751-robot-collisions.cpp
@@ -0,0 +1,49 @@
+class Solution {
+public:
+    vector<int> survivedRobotsHealths(vector<int>& positions, vector<int>& healths, string directions) {
+        int n =positions.size();
+        vector<int> actual_index(n);
+
+
+        for(int i=0;i<n;i++){
+            actual_index[i] = i;
+        }
+        auto lambda =[&](int &i ,int &j){
+                return positions[i] < positions[j];
+        }; 
+
+        sort(begin(actual_index) ,end(actual_index),lambda);
+
+        vector<int> result;
+        stack<int> s;
+
+        for(int &currIdx : actual_index){
+            if(directions[currIdx] == 'R'){
+                s.push(currIdx);
+            }else{
+                while(!s.empty() && healths[currIdx] >0){
+                    int top_idx = s.top();
+                    s.pop();
+
+                    if(healths[top_idx] > healths[currIdx]){
+                        healths[top_idx] -=1;
+                        healths[currIdx] =0;
+                        s.push(top_idx);
+                    }else if(healths[top_idx] < healths[currIdx]){
+                        healths[top_idx] =0;
+                        healths[currIdx] -=1;
+                    }else{
+                        healths[top_idx] =0;
+                        healths[currIdx] =0;
+                    }
+                }
+            }
+        }
+        for(int i=0;i<n;i++){
+            if(healths[i] >0){
+                result.push_back(healths[i]);
+            }
+        }
+        return result;
+    }
+};
