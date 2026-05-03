818114b77f77e1638f52fe591d634152fef29f3d
Sukumar Parida <sukumarparida77@gmail.com>
2026-05-03 15:55:39 +0530

    Time: 1 ms (63.59%), Space: 17.9 MB (83.97%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
@@ -1,33 +1,38 @@
 class Solution {
 public:
 
-    struct cmp{
-        bool operator()(pair<int,int>&a , pair<int,int>&b){
-            if(a.first != b.first)
-                return a.first > b.first;
-            return a.second > b.second;
+struct cmp{
+    bool operator()(pair<int,int>&a ,pair<int,int>&b){
+        if(a.first != b.first){
+            return a.first > b.first;
         }
-    };
+        return a.second > b.second;
+    }
+};
     vector<int> topKFrequent(vector<int>& nums, int k) {
-        priority_queue<pair<int,int>,vector<pair<int,int>> ,cmp> pq;
-        unordered_map<int,int> m;
-        for(int i=0;i<nums.size();i++){
-            m[nums[i]]++;
+        int n =nums.size();
+        priority_queue<pair<int,int>,vector<pair<int,int>>,cmp>pq;
+        unordered_map<int,int>H;
+        for(int i=0;i<n;i++){
+            H[nums[i]]++;
         }
-        for(auto i : m){
-            int element = i.first;
-            int freq = i.second;
-        pair<int,int>curr {freq ,element};
-        if(pq.size() <k){
-            pq.push(curr);
-            continue;
-        }
+        for(auto i : H){
+            int ele = i.first;
+            int freq =i.second;
+
+            pair<int,int> curr{freq ,ele};
+
+            if(pq.size()<k){
+                pq.push(curr);
+                continue;
+            }
             if(curr.first < pq.top().first)
             continue;
+
             pq.pop();
             pq.push(curr);
         }
-        vector<int> res;
+        vector<int>res;
         while(!pq.empty()){
             res.push_back(pq.top().second);
             pq.pop();
