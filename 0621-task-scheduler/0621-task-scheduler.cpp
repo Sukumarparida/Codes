4757ad82526ed04cfb7409aec9a1bfaa7ea03758
Sukumar Parida <sukumarparida77@gmail.com>
2026-05-09 11:13:12 +0530

    Time: 1884 ms (5%), Space: 459.1 MB (5.01%) - LeetHub

--- 0621-task-scheduler/0621-task-scheduler.cpp
+++ 0621-task-scheduler/0621-task-scheduler.cpp
@@ -1,41 +1,36 @@
 class Solution {
 public:
-
     int leastInterval(vector<char>& tasks, int n) {
-
-        unordered_map<char,int> freq;
+        unordered_map<char,int>freq;
         unordered_map<char,int>free;
         for(int i=0;i<tasks.size();i++){
             freq[tasks[i]]++;
             free[tasks[i]] =1;
         }
-        priority_queue<pair<int,char>>pq;
-        int seat =1;
+        priority_queue<pair<int,char>> pq;
         for(auto i : freq){
-            int freq =i.second;
-            char child = i.first;
-            pq.push({freq,child});
+            pq.push({i.second , i.first});
         }
+        int seat =1;
         while(!pq.empty()){
-            vector<pair<int,char>>pulled;
+            vector<pair<int,char>> pulled;
             while(!pq.empty()){
                 pair<int,char> p =pq.top();
                 pq.pop();
-                int fr =p.first;
-                int child =p.second;
+                int fre =p.first;
+                char child=p.second;
                 if(free[child] <= seat){
                     if(p.first >1)
-                        pq.push({p.first-1,p.second});
-                        free[p.second] = seat+n+1;
-                     break;   
+                    pq.push({p.first-1,p.second});
+                    free[child] =seat + n + 1;
+                    break;
                 }else{
                     pulled.push_back(p);
                 }
             }
-            for(int i=0;i<pulled.size();i++){
-                  pq.push(pulled[i]);
-            }
-         seat++;
+            seat++;
+            for(int i=0;i<pulled.size();i++)
+            pq.push(pulled[i]);
         }
         return seat-1;
     }
